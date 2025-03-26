10e50aa78996fe145bf10ee1d3477fe70ca9a848
jaypal <jaypalrathod7147@gmail.com>
2025-03-26 16:14:32 +0530

    office push

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -3,9 +3,16 @@ import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@store/reduxHook';
 import { getHomeContent } from './api/actions';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 const Home = () => {
   const insets = useSafeAreaInsets();
+
+  const scrollYGlobal = useSharedValue(0)
+
+  const moveUpStyle = useAnimatedStyle(() => { 
+
+  });
 
   return (
     <View style={styles.container}>
